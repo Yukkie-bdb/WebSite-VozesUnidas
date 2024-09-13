742163e30607139b9678aad1ae070b76c1e74b6f
eRIC bROLIO <ericbrolio@gmail.com>
2024-09-13 13:39:35 -0300

    User model bom

---
+++ WebSiteVozesUnidas/Data/Migrations/20240913141825_AdicionandoApplicationUser.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebSiteVozesUnidas.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoApplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Foto",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdUsuario",
+                table: "AspNetUsers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nome",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Senha",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sobre",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Telefone",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Tipo",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "tbMidiaSocial",
+                columns: table => new
+                {
+                    IdMidiaSocial = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Plataforma = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApplicationUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbMidiaSocial", x => x.IdMidiaSocial);
+                    table.ForeignKey(
+                        name: "FK_tbMidiaSocial_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tbMidiaSocial_ApplicationUserId",
+                table: "tbMidiaSocial",
+                column: "ApplicationUserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbMidiaSocial");
+
+            migrationBuilder.DropColumn(
+                name: "Foto",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdUsuario",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nome",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Senha",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Sobre",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Telefone",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Tipo",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+        }
+    }
+}
---
+++ WebSiteVozesUnidas/Models/ApplicationUser.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebSiteVozesUnidas.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? Foto { get; set; }
+        public string? Sobre { get; set; }
+        public TipoUsuario Tipo { get; set; }
+        public List<MidiaSocial> MidiaSocials { get; set; } = new List<MidiaSocial>();
+
+        // Propriedades específicas para PessoaFisica
+        public string? CPF { get; set; }
+        public DateOnly? Nascimento { get; set; }
+        public string[]? Habilidades { get; set; }
+        public string? Objetivos { get; set; }
+        public bool? Jornalista { get; set; }
+
+        // Propriedades específicas para Empresa
+        public string? CNPJ { get; set; }
+    }
+    public enum TipoUsuario
+    {
+        PessoaFisica,
+        Empresa,
+        ADM
+    }
+    public class MidiaSocial
+    {
+        public int IdMidiaSocial { get; set; }
+        public string Plataforma { get; set; } // Nome da rede social (ex: Twitter, Instagram)
+        public string Url { get; set; } // URL da conta
+    }
+}
