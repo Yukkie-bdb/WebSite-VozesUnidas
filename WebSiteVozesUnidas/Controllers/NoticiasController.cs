64afbe49b9a6c26caf47aa9367f585039dd1d2d3
Hamanameia <hamanameia1@gmail.com>
2024-09-27 15:33:09 -0300

    Noticia em andamento

---
+++ WebSiteVozesUnidas/Controllers/NoticiasController.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebSiteVozesUnidas.Data;
+using WebSiteVozesUnidas.Models;
+
+namespace WebSiteVozesUnidas.Controllers
+{
+    public class NoticiasController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private string _caminho;
+
+        public NoticiasController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _caminho = hostEnvironment.WebRootPath;
+
+        }
+
+        public List<Noticia> GetRandomItems(int count)
+        {
+
+            var allIds = _context.Noticias.Select(x => x.IdNoticia).ToList();
+            var random = new Random();
+
+            var randomIds = allIds.OrderBy(x => random.Next()).Take(count).ToList();
+
+            var randomItems = _context.Noticias 
+                                       .Where(x => randomIds.Contains(x.IdNoticia))
+                                       .ToList();
+
+            return randomItems;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var randomItems = GetRandomItems(3).ToList();
+            var i = 0;
+            foreach (var item in randomItems)
+                {
+                    ViewData[i.ToString()] = item.Imagem;
+                    ViewData[$"{i}Id"] = item.IdNoticia;
+                    i++;
+                }
+            
+
+            return View(await _context.Noticias.ToListAsync());
+        }
+
+
+
+        // GET: Noticias/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noticia = await _context.Noticias
+                .FirstOrDefaultAsync(m => m.IdNoticia == id);
+            if (noticia == null)
+            {
+                return NotFound();
+            }
+
+            return View(noticia);
+        }
+
+        // GET: Noticias/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Noticias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdNoticia,Titulo,Conteudo,Publicacao,IdUsuario")] Noticia noticia, IFormFile imgUp)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                noticia.IdNoticia = Guid.NewGuid();
+                noticia.IdUsuario = Guid.Parse(userId);
+                noticia.Publicacao = DateTime.Now;
+                if (imgUp != null && imgUp.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_caminho, "img");
+
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + imgUp.FileName;
+
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await imgUp.CopyToAsync(fileStream);
+                    }
+                    noticia.Imagem = uniqueFileName;
+                }
+                _context.Add(noticia);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(noticia);
+        }
+    
+
+        // GET: Noticias/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noticia = await _context.Noticias.FindAsync(id);
+            if (noticia == null)
+            {
+                return NotFound();
+            }
+            return View(noticia);
+        }
+
+        // POST: Noticias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("IdNoticia,Titulo,Imagem,Conteudo,Publicacao,IdUsuario")] Noticia noticia)
+        {
+            if (id != noticia.IdNoticia)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(noticia);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NoticiaExists(noticia.IdNoticia))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(noticia);
+        }
+
+        // GET: Noticias/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var noticia = await _context.Noticias
+                .FirstOrDefaultAsync(m => m.IdNoticia == id);
+            if (noticia == null)
+            {
+                return NotFound();
+            }
+
+            return View(noticia);
+        }
+
+        // POST: Noticias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var noticia = await _context.Noticias.FindAsync(id);
+            if (noticia != null)
+            {
+                _context.Noticias.Remove(noticia);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NoticiaExists(Guid id)
+        {
+            return _context.Noticias.Any(e => e.IdNoticia == id);
+        }
+    }
+}
--- WebSiteVozesUnidas/Models/Noticia.cs
+++ WebSiteVozesUnidas/Models/Noticia.cs
@@ -1,14 +1,36 @@
-﻿namespace WebSiteVozesUnidas.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebSiteVozesUnidas.Models
 {
     public class Noticia
     {
         public Guid IdNoticia { get; set; }
         public string Titulo { get; set; }
-        public string Imagem { get; set; }
-        public string Resumo { get; set; }
+        public string? Imagem { get; set; }
+        public string Resumo => ResumirTexto(Conteudo, 540);
+        
         public string Conteudo { get; set; }
-        DateTime Publicacao { get; set; }
+        public DateTime Publicacao { get; set; }
         public Guid IdUsuario { get; set; }
         public ApplicationUser? Usuario { get; set; }
+
+        private string ResumirTexto(string texto, int limiteCaracteres)
+        {
+            if(string.IsNullOrEmpty(texto))
+            return string.Empty;
+
+            if (texto.Length <= limiteCaracteres)
+                return texto;
+
+            string resumo = texto.Substring(0, limiteCaracteres);
+
+            int ultimoEspaco = resumo.LastIndexOf(' ');
+            if (ultimoEspaco > 0)
+                resumo = resumo.Substring(0, ultimoEspaco);
+
+            return resumo + "...";
+        }
+
+
     }
 }
