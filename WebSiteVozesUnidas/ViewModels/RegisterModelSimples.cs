5ef13e260b69fbc337e5683143b1debbaf76bce9
eRIC bROLIO <ericbrolio@gmail.com>
2024-10-24 11:45:21 -0300

    Register ViewPage

---
+++ WebSiteVozesUnidas/ViewModels/RegisterModelSimples.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.V4.Pages.Account.Internal;
+
+namespace WebSiteVozesUnidas.Models
+{
+    public class RegisterModelSimples
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string? ReturnUrl { get; internal set; }
+    }
+}
