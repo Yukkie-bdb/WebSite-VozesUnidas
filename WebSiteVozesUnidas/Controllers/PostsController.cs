ddf277be2ce6a09d3fb1c6e2c5bce0ba3071e8c3
Hamanameia <hamanameia1@gmail.com>
2024-10-25 15:35:00 -0300

    Forum Andamento

--- WebSiteVozesUnidas/Controllers/PostsController.cs
+++ WebSiteVozesUnidas/Controllers/PostsController.cs
@@ -1,27 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebSiteVozesUnidas.Data;
 using WebSiteVozesUnidas.Models;
+using static Azure.Core.HttpHeader;
 
 namespace WebSiteVozesUnidas.Controllers
 {
     public class PostsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private string _caminho;
 
-        public PostsController(ApplicationDbContext context)
+        public PostsController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment, SignInManager<ApplicationUser> signInManager)
         {
             _context = context;
+            _signInManager = signInManager;
+            _caminho = hostEnvironment.WebRootPath;
+
         }
 
         // GET: Posts
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(Guid? id)
         {
+            
+            var Categorias = await _context.CategoriaPosts.ToListAsync();
+            ViewBag.Categorias = Categorias;
+            if(id != null)
+            {
+                var tudo = await _context.Posts.ToListAsync();
+                var filtrado = new List<Post>();
+                foreach(var item in tudo)
+                {
+
+                    if(item.IdCategoria == id)
+                    {
+                        filtrado.Add(item);
+                    }
+                }
+                return View(filtrado);
+
+            }
+
             return View(await _context.Posts.ToListAsync());
         }
 
@@ -33,8 +61,16 @@ namespace WebSiteVozesUnidas.Controllers
                 return NotFound();
             }
 
-            var post = await _context.Posts
-                .FirstOrDefaultAsync(m => m.IdPost == id);
+            var post = await _context.Posts.FirstOrDefaultAsync(m => m.IdPost == id);
+            ViewBag.Image = post.Imagem;    
+            var Categorias = await _context.CategoriaPosts.ToListAsync();
+            foreach(var item in Categorias)
+            {
+                if(item.IdCategoriaPost == post.IdCategoria)
+                {
+                    ViewBag.Categoria = item.Nome;
+                }
+            }
             if (post == null)
             {
                 return NotFound();
@@ -44,8 +80,22 @@ namespace WebSiteVozesUnidas.Controllers
         }
 
         // GET: Posts/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
+            var userId = _signInManager.UserManager.GetUserId(User);
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
+            var Categorias = await _context.CategoriaPosts.ToListAsync();
+
+            ViewBag.User = user;
+            ViewBag.Categorias = Categorias;
+            if(userId is null)
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            if (ViewBag.categorias == null)
+            {
+                ViewBag.categorias = new List<CategoriaPost>();
+            }
             return View();
         }
 
@@ -54,11 +104,33 @@ namespace WebSiteVozesUnidas.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdPost,Titulo,SubTituloResumo,Conteudo,Imagem,Publicacao,IdUsuario,IdCategoria")] Post post)
+        public async Task<IActionResult> Create([Bind("IdPost,Titulo,SubTituloResumo,Conteudo,Publicacao,IdUsuario,IdCategoria")] Post post, IFormFile? imgUp)
         {
             if (ModelState.IsValid)
             {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                post.IdUsuario = Guid.Parse(userId);
+                post.Publicacao = DateTime.Now;
                 post.IdPost = Guid.NewGuid();
+                if (imgUp != null && imgUp.Length > 0)
+                {
+                    string uploadsFolder = Path.Combine(_caminho, "img");
+
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    string uniqueFileName = Guid.NewGuid().ToString() + "_" + imgUp.FileName;
+
+                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await imgUp.CopyToAsync(fileStream);
+                    }
+                    post.Imagem = uniqueFileName;
+                }
                 _context.Add(post);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -138,16 +210,38 @@ namespace WebSiteVozesUnidas.Controllers
         // POST: Posts/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        public async Task<IActionResult> DeleteConfirmed(Guid id, string? name)
         {
-            var post = await _context.Posts.FindAsync(id);
-            if (post != null)
+            var noticia = await _context.Noticias.FindAsync(id);
+            if (name == "Quero Apagar")
             {
-                _context.Posts.Remove(post);
+                if (noticia != null)
+                {
+                    _context.Noticias.Remove(noticia);
+                }
+                string uploadsFolder = Path.Combine(_caminho, "img");
+
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                string uniqueFileName = noticia.Imagem;
+
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                noticia.Imagem = uniqueFileName;
+                if (System.IO.File.Exists(filePath))
+                {
+                    System.IO.File.Delete(filePath);
+                }
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+
             }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
         private bool PostExists(Guid id)
--- WebSiteVozesUnidas/Models/Post.cs
+++ WebSiteVozesUnidas/Models/Post.cs
@@ -6,9 +6,10 @@ namespace WebSiteVozesUnidas.Models
     {
         public Guid IdPost { get; set; }
         public string Titulo { get; set; }
-        public string SubTituloResumo { get; set; }
+        public string SubTituloResumo => ResumirTexto(Conteudo, 500);
+
         public string Conteudo { get; set; }
-        public string Imagem { get; set; }
+        public string? Imagem { get; set; }
         public DateTime Publicacao { get; set; }
         public Guid? IdUsuario { get; set; }
         public ApplicationUser? Usuario { get; set; }
@@ -16,7 +17,22 @@ namespace WebSiteVozesUnidas.Models
         public CategoriaPost? CategoriaPost { get; set; }
         public IEnumerable<Comentario>? Comentarios { get; set; }
         public IEnumerable<LikesPost>? Likes { get; set; }
+        private string ResumirTexto(string texto, int limiteCaracteres)
+        {
+            if (string.IsNullOrEmpty(texto))
+                return string.Empty;
 
+            if (texto.Length <= limiteCaracteres)
+                return texto;
+
+            string resumo = texto.Substring(0, limiteCaracteres);
+
+            int ultimoEspaco = resumo.LastIndexOf(' ');
+            if (ultimoEspaco > 0)
+                resumo = resumo.Substring(0, ultimoEspaco);
+
+            return resumo + "...";
+        }
     }
     public class LikesPost
     {
