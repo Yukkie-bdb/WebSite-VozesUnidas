aa786ce839164fce30fb82ff51c5d1389ad2ed5b
Eric <ericbrolio@gmail.com>
2024-11-17 04:28:14 -0300

    Home meio caminho andado

--- WebSiteVozesUnidas/Controllers/HomeController.cs
+++ WebSiteVozesUnidas/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebSiteVozesUnidas.Data;
 using WebSiteVozesUnidas.Models;
 using WebSiteVozesUnidas.ViewModels;
 
@@ -9,65 +11,72 @@ namespace WebSiteVozesUnidas.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
         private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(UserManager<ApplicationUser> userManager, ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger<HomeController> logger)
         {
             _userManager = userManager;
             _logger = logger;
+            _context = context;
+
         }
 
 
         public async Task<IActionResult> Index()
         {
-            //var user = await _userManager.GetUserAsync(User);
-            //if (user == null)
-            //{
-            //    return RedirectToAction("Index", "Noticias");
-            //}
+            var especialistasComMedia = _context.Especialistas
+        .Select(e => new
+        {
+            e.IdEspecialista,
+            e.ImgEspecialista,
+            e.Nome,
+            e.Telefone,
+            e.Email,
+            e.Especialhidade,
+            MediaEstrelas = e.AvaliacoesEspecialistas.Any()
+                ? e.AvaliacoesEspecialistas.Average(a => a.Estrelas)
+                : 0
+        })
+        .OrderByDescending(e => e.MediaEstrelas)
+        .Take(3)  // Limita aos 3 primeiros
+        .ToList();
 
-            //var model = new UserDebugViewModel
-            //{
-            //    // Campos padrão do Identity
-            //    Id = user.Id,
-            //    UserName = user.UserName,
-            //    Email = user.Email,
-            //    PhoneNumber = user.PhoneNumber,
-            //    EmailConfirmed = user.EmailConfirmed,
+            //ViewBag.especialistasList = await _context.Especialistas.OrderBy().Take(3).ToListAsync();
+            ViewBag.especialistasList = especialistasComMedia;
 
-            //    // Campos personalizados de ApplicationUser
-            //    Foto = user.Foto,
-            //    Sobre = user.Sobre,
-            //    Portifolio = user.Portifolio,
-            //    Tipo = user.Tipo,
-            //    MidiaSocials = user.MidiaSocials,
-            //    Estado = user.Estado,
-            //    Cidade = user.Cidade,
-            //    CPF = user.CPF,
-            //    Nascimento = user.Nascimento,
-            //    Habilidades = user.Habilidades,
-            //    Objetivos = user.Objetivos,
-            //    Jornalista = user.Jornalista,
-            //    CNPJ = user.CNPJ,
-            //    Ramo = user.Ramo,
-            //    Funcionarios = user.Funcionarios,
-            //    Noticias = user.Noticias,
-            //    AvaliacoesEspecialista = user.AvaliacoesEspecialista,
-            //    Posts = user.Posts,
-            //    Comentarios = user.Comentarios,
-            //    LikesPosts = user.LikesPosts,
-            //    CandidatoVagas = user.CandidatoVagas,
-            //    VagaEmpregos = user.VagaEmpregos,
-            //    CandidatosJornalistass = user.CandidatosJornalistass
-            //};
+            ViewBag.noticiasList = await _context.Noticias.ToListAsync();
 
-            //return View(model);
+
+            var materiaisPorCategoria = await _context.MaterialDidaticos
+                   .Include(m => m.Categoria) // Inclui a categoria relacionada
+                   .GroupBy(m => m.Categoria.Categoria) // Agrupa por categoria
+                   .ToListAsync();
+
+            var categoriasComMateriais = materiaisPorCategoria
+                .Select(group => new CategoriaMaterials
+                {
+                    Categoria = group.Key,
+                    MaterialDidaticos = group
+                })
+                .ToList();
+
+            ViewBag.CategoriasComMateriais = categoriasComMateriais;
+
+            ViewBag.empregos = await _context.VagaEmpregos.ToListAsync();
+
+
 
             return View();
         }
 
         public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult APAGAR()
         {
             return View();
         }
